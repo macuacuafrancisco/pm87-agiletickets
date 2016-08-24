a93f5fc8f8beb1599477b5c73bc3759219e28474
Francisco Macuacua <macuacuafrancisco>
2016-08-24 09:19:22 +0200

    Modificando o codigo/ refactoracao

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -9,38 +9,32 @@ public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
-		
-		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
-			//quando estiver acabando os ingressos... 
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.05) { 
+
+		int totalDisponivel = sessao.getTotalIngressos() - sessao.getIngressosReservados();
+		double totalIngressos = sessao.getTotalIngressos().doubleValue();
+		TipoDeEspetaculo tipoEspetaculo = sessao.getEspetaculo().getTipo();
+		if (tipoEspetaculo.equals(TipoDeEspetaculo.CINEMA) || tipoEspetaculo.equals(TipoDeEspetaculo.SHOW)) {
+			// quando estiver acabando os ingressos...
+			if (totalDisponivel / totalIngressos <= 0.05) {
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			} else {
 				preco = sessao.getPreco();
 			}
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-			} else {
-				preco = sessao.getPreco();
-			}
-			
-			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			}
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
+		} else if (tipoEspetaculo.equals(TipoDeEspetaculo.BALLET)
+				|| tipoEspetaculo.equals(TipoDeEspetaculo.ORQUESTRA)) {
+			if (totalDisponivel / totalIngressos <= 0.50) {
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
 			} else {
 				preco = sessao.getPreco();
 			}
 
-			if(sessao.getDuracaoEmMinutos() > 60){
+			if (sessao.getDuracaoEmMinutos() > 60) {
 				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			}
-		}  else {
-			//nao aplica aumento para teatro (quem vai é pobretão)
+		} else {
+			// nao aplica aumento para teatro (quem vai é pobretão)
 			preco = sessao.getPreco();
-		} 
+		}
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
